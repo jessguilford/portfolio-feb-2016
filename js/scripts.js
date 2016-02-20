d3e2ac2e9bfb1339941909e9d0b6731a18588604
Jessie Guilford <Jessie Guilford>
2016-02-19 18:41:41 -0800

    add footer

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,4 @@ $(document).ready(function() {
     $("#info-" + this.id).fadeToggle(200);
     $("#x-toggle-" + this.id).toggleClass("item-x-rotated");
   });
-
-  // $(".item-info").click(function() {
-  //   $(this).slideToggle(400);
-  //   $(".item-x-toggle").toggleClass("item-x-rotated");
-  // });
 });
-//
-// $("button").click(function(event) {
-//     $("#text" + this.id).fadeToggle(2000);
-//     $("#text" + this.id).siblings().hide();
