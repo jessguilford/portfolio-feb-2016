301eac020ef4bed7327130caf217360cf2627c4f
Jessie Guilford <Jessie Guilford>
2016-02-19 18:21:44 -0800

    fix rotation issue by changing item ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   $(".item-label").click(function(event) {
     $("#info-" + this.id).fadeToggle(200);
-    $(".item-x-toggle").toggleClass("item-x-rotated");
+    $("#x-toggle-" + this.id).toggleClass("item-x-rotated");
   });
 
-  $(".item-info").click(function() {
-    $(this).slideToggle(400);
-  });
+  // $(".item-info").click(function() {
+  //   $(this).slideToggle(400);
+  //   $(".item-x-toggle").toggleClass("item-x-rotated");
+  // });
 });
 //
 // $("button").click(function(event) {
